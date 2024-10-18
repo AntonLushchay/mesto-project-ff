26792f8364e6c6ade16253f98189d54e8ce20de8
Anton <av.lushchay17@gmail.com>
2024-10-18 13:22:28 +0200

    I DID IT!

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+import { initialCards } from './cards.js';
 
-// @todo: DOM узлы
+function createCard(card, deleteCardCallback) {
+	const cardTemplate = document.querySelector('#card-template').content;
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+	cardElement.querySelector('.card__image').src = card.link;
+	cardElement.querySelector('.card__image').alt = card.name;
+	cardElement.querySelector('.card__title').textContent = card.name;
 
-// @todo: Функция создания карточки
+	const deleteButton = cardElement.querySelector('.card__delete-button');
+	deleteButton.addEventListener('click', () => deleteCardCallback(cardElement));
 
-// @todo: Функция удаления карточки
+	return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+function deleteCard(cardElement) {
+	cardElement.remove();
+}
+
+initialCards.forEach((card) => {
+	const cardElement = createCard(card, deleteCard);
+	document.querySelector('.places__list').prepend(cardElement);
+});
