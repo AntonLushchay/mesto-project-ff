92d42315ebb9b97c0d86b7ef5185f59fb881c4e6
AntonLushchay <av.lushchay17@gmail.com>
2025-12-06 03:21:57 +0100

    fix: add source map support for better debugging in development mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,68 +4,69 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	entry: { main: './src/index.js' },
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.js',
-		publicPath: '',
-	},
-	mode: 'development',
-	devServer: {
-		static: path.resolve(__dirname, 'dist'),
-		compress: true,
-		port: 8080,
-		open: true,
-		devMiddleware: {
-			writeToDisk: true,
-		},
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: 'babel-loader',
-				exclude: '/node_modules/',
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: 'asset/resource',
-				generator: {
-					filename: 'images/[name][ext]',
-				},
-			},
-			{
-				test: /\.(mp3|wav|ogg)$/i,
-				type: 'asset/resource',
-				generator: {
-					filename: 'audio/[name][ext]',
-				},
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				type: 'asset/resource',
-				generator: {
-					filename: 'fonts/[name][ext]',
-				},
-			},
-			{
-				test: /\.(scss|css)$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					{
-						loader: 'css-loader',
-						options: { importLoaders: 1 },
-					},
-					'postcss-loader',
-				],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-		}),
-		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin(),
-	],
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: '',
+    },
+    mode: 'development',
+    devtool: 'eval-source-map',
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        compress: true,
+        port: 8080,
+        open: true,
+        devMiddleware: {
+            writeToDisk: true,
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: '/node_modules/',
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
+                },
+            },
+            {
+                test: /\.(mp3|wav|ogg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'audio/[name][ext]',
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]',
+                },
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { importLoaders: 1 },
+                    },
+                    'postcss-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+    ],
 };
